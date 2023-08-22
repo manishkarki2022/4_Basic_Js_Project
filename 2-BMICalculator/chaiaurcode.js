c14a42241f795d0a526adf932a23fc4e2c047d02
manishkarki2022 <np03cs4s210031@heraldcollege.edu.np>
2023-08-22 18:23:01 +0545

    BMI code Complete

---
+++ 2-BMICalculator/chaiaurcode.js
@@ -0,0 +1,22 @@
+const form =  document.querySelector('form');
+form.addEventListener('submit', function(e){
+    e.preventDefault(); 
+    const height = Number(document.querySelector('#height').value)
+    const weight = Number(document.querySelector('#weight').value)
+    const results = document.querySelector('#results');
+    if(height==''||height<0 ||isNaN(height)){
+        results.innerHTML =`Please give valid height ${height}` 
+
+    }
+    else if(weight==''||weight<0 ||isNaN(weight)){
+        results.innerHTML =`Please give valid weight ${weight}` 
+
+    }else{
+        let bmi= (weight/(Math.pow((height/100),2))).toFixed() ;
+        results.innerHTML =`Your BMI is ${bmi}`
+    }
+    
+    
+
+
+})
