31b82ebceaa1489baea6c824977bfc301a39e7ec
manishkarki2022 <np03cs4s210031@heraldcollege.edu.np>
2023-08-22 18:34:59 +0545

    Updated BMI add under over msg for user

--- 2-BMICalculator/chaiaurcode.js
+++ 2-BMICalculator/chaiaurcode.js
@@ -1,22 +1,21 @@
-const form =  document.querySelector('form');
-form.addEventListener('submit', function(e){
+const form = document.querySelector('form');
+form.addEventListener('submit', function(e) {
     e.preventDefault(); 
-    const height = Number(document.querySelector('#height').value)
-    const weight = Number(document.querySelector('#weight').value)
+    const height = Number(document.querySelector('#height').value);
+    const weight = Number(document.querySelector('#weight').value);
     const results = document.querySelector('#results');
-    if(height==''||height<0 ||isNaN(height)){
-        results.innerHTML =`Please give valid height ${height}` 
-
-    }
-    else if(weight==''||weight<0 ||isNaN(weight)){
-        results.innerHTML =`Please give valid weight ${weight}` 
-
-    }else{
-        let bmi= (weight/(Math.pow((height/100),2))).toFixed() ;
-        results.innerHTML =`<span>Your BMI is ${bmi}</span>`
-    }
     
-    
-
-
-})
+    if (isNaN(height) || isNaN(weight) || height <= 0 || weight <= 0) {
+        results.innerHTML = "Please provide valid height and weight.";
+    } else {
+        const bmi = (weight / (Math.pow(height / 100, 2)));
+        
+        if (bmi <= 18.6) {
+            results.innerHTML = `<span>Your BMI is ${bmi.toFixed(1)} Under Weight</span>`;
+        } else if (bmi > 18.6 && bmi <= 24.9) {
+            results.innerHTML = `<span>Your BMI is ${bmi.toFixed(1)} Normal Weight</span>`;
+        } else {
+            results.innerHTML = `<span>Your BMI is ${bmi.toFixed(1)} Over Weight</span>`;
+        }
+    }
+});
